ca8f2bf0095d2436d776e776734b1f6bf96bbd5e
rohitgubrele <rohitgubrele8@gmail.com>
2018-06-26 09:50:53 +0530

    Before Catagory Spinner

--- app/src/main/java/com/example/vedantiladda/quiz/LoginActivity.java
+++ app/src/main/java/com/example/vedantiladda/quiz/LoginActivity.java
@@ -3,11 +3,16 @@ package com.example.vedantiladda.quiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
 
+public class LoginActivity extends AppCompatActivity {
+    private Retrofit retrofit;
+    private OkHttpClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
